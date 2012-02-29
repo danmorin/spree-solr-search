7d83c318c67e072b4d0a9535de03c8e2426351e5
Dan Morin <dan.morin@gmail.com>
2012-02-28 16:25:32 -0800

    Removing adding facets in this extension

--- app/overrides/show_search_partials.rb
+++ app/overrides/show_search_partials.rb
@@ -1,8 +1,9 @@
-Deface::Override.new(:virtual_path => "spree/shared/_taxonomies",
-                      :name => "show_search_partials_facets",
-                      :insert_top => "#taxonomies",
-                      :partial => "spree/products/facets",
-                      :disabled => false)
+# Adding on our side
+# Deface::Override.new(:virtual_path => "spree/shared/_taxonomies",
+#                       :name => "show_search_partials_facets",
+#                       :insert_top => "#taxonomies",
+#                       :partial => "spree/products/facets",
+#                       :disabled => false)
                      
 Deface::Override.new(:virtual_path => "spree/products/index",
                       :name => "show_search_partials_suggestion",
